3ea6e3c5f637b173af420330e05da724a9f2460b
PARKYOONJUNG6163 <201602001@o.cnu.ac.kr>
2019-01-15 17:47:56 +0900

    data가져와서 기사만 추출

---
+++ SentimentAnalysis.py
@@ -0,0 +1,36 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+import re
+from nltk.corpus import stopwords
+
+import pymysql
+
+
+# In[16]:
+
+
+def get_article_body() :
+    conn = pymysql.connect(host = "147.43.122.131", user = "root", password = "1234", charset = "utf8")
+    curs = conn.cursor()
+    curs.execute("use yahoo_news ;")
+    query = """select * from galaxys; """
+    curs.execute(query)
+    all_rows = curs.fetchall()
+    body_list = []
+    for i in all_rows:
+        temp = re.sub('<.+?>', '', i[7], 0).strip() # 태그 제거
+        temp = re.sub(r'\\x..', '', temp) #\xa0 같은 거 제거
+        body_list.append(temp)
+    print(body_list)
+
+
+# In[17]:
+
+
+body_list = []
+get_article_body()
+
